93752d05041194bf13310fd7bde4b46a99544451
xiaoming <xiaoming@mgtv.com>
2024-09-27 11:31:57 +0800

    feat update dependences

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  future: { compatibilityVersion: 4 },
   modules: [
     'shadcn-nuxt',
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/seo',
   ],
   shadcn: {
     /**

a19b6f5e068e78a8f638d9f0853976b717cc57cd
xiaoming <xiaoming@mgtv.com>
2024-09-27 11:23:20 +0800

    feat update dependences

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,12 @@
-import { defineNuxtConfig } from 'nuxt/config'
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
   modules: [
     'shadcn-nuxt',
     '@nuxtjs/tailwindcss',
     '@nuxtjs/seo',
-    '@nuxtjs/robots' // 添加这个模块
   ],
-
   shadcn: {
     /**
      * Prefix for all the imported component
@@ -18,73 +17,5 @@ export default defineNuxtConfig({
      * @default "./components/ui"
      */
     componentDir: './components/ui'
-  },
-
-  app: {
-    pageTransition: { name: 'page', mode: 'out-in' },
-    head: {
-      htmlAttrs: {
-        lang: 'en',
-        class: 'dark',
-      },
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-    },
-    baseURL: '/', // 确保这与你的 Cloudflare Pages 设置匹配
-    buildAssetsDir: '/_nuxt/', // 这是默认值，你可以根据需要更改
-  },
-
-  nitro: {
-    prerender: {
-      crawlLinks: true,
-      routes: ['/'],
-    },
-    static: true,
-  },
-
-  vite: {
-    build: {
-      assetsDir: '/_nuxt', // 确保这与 app.buildAssetsDir 匹配
-    },
-  },
-
-  compatibilityDate: '2024-09-26',
-  build: {
-    extractCSS: true,
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          styles: {
-            name: 'styles',
-            test: /\.(css|vue)$/,
-            chunks: 'all',
-            enforce: true
-          }
-        }
-      }
-    }
-  },
-
-  image: {
-    dir: 'assets/images',
-    // 你可以添加更多图片优化配置
-  },
-
-  googleFonts: {
-    families: {
-      Inter: [400, 700],
-      // 添加其他你需要的字体
-    },
-    display: 'swap',
-    preload: true,
-  },
-
-  tailwindcss: {
-    cssPath: '~/assets/css/tailwind.css',
-    configPath: 'tailwind.config.js',
-    exposeConfig: false,
-    injectPosition: 0,
-    viewer: true,
-  },
+  }
 })
-
